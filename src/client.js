58cdcd27920a8b78bc795fec3ffc980a7d114612
Brett Cunningham <bwcunninghamii@gmail.com>
2019-08-22 11:04:13 -0400

    added comments explaining lines up to Router

--- src/client.js
+++ src/client.js
@@ -4,4 +4,5 @@ import ReactDOM from "react-dom";
 import Layout from "./components/Layout";
 
 const app = document.getElementById( "app" );
+// this will attach any event handlers we had in our react app
 ReactDOM.hydrate( <Layout />, app );
--- src/server.js
+++ src/server.js
@@ -1,16 +1,24 @@
 import express from "express";
 import path from "path";
-
+import { StaticRouter } from "react-router-dom";
 import React from "react";
 import { renderToString } from "react-dom/server";
 import Layout from "./components/Layout";
 
 const app = express();
 
+// serves static files to the client
 app.use( express.static( path.resolve( __dirname, "../dist" ) ) );
 
+// at all routes
+
 app.get( "/*", ( req, res ) => {
-    const jsx = ( <Layout /> );
+    const context = { };
+    const jsx = (
+        <StaticRouter context={ context } location={ req.url }>
+            <Layout />
+        </StaticRouter>
+    );
     const reactDom = renderToString( jsx );
 
     res.writeHead( 200, { "Content-Type": "text/html" } );
@@ -35,3 +43,18 @@ function htmlTemplate( reactDom ) {
         </html>
     `;
 }
+
+// app.get( "/*", ( req, res ) => {
+//     // layout is just a single page with a h1 tag as welcome to React SSR
+//     const jsx = ( <Layout /> );
+//     // this is converted to a string https://reactjs.org/docs/react-dom-server.html#rendertostring
+//     // this method generates HTML on the server
+//     const reactDom = renderToString( jsx );
+
+//     // you have to write the headers (settings for the information you are sending)
+//     // all we are doing is generating the final html code that will be used on the server
+//     // so when this is sent to the client it just needs to use the sent HTML
+//     res.writeHead( 200, { "Content-Type": "text/html" } );
+//     // put the JSX code into the HTML template and send it to the client.
+//     res.end( htmlTemplate( reactDom ) );
+// } );
